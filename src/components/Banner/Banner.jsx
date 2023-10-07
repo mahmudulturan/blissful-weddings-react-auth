3db383118b2f2ae77ad3513dc6ad3246402166a2
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2023-10-07 12:48:59 +0600

    Add: Banner with slider

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import BannerSlider from "./BannerSlider";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/pagination';
+
+// import required modules
+import { EffectFade, Pagination, Autoplay } from 'swiper/modules';
+
+const Banner = () => {
+    const [bannerData, setBannerData] = useState([]);
+    useEffect(()=>{
+        fetch('/banner.json')
+        .then(res=> res.json())
+        .then(data=> setBannerData(data))
+    },[])
+
+
+    
+    return (
+        
+        <div>
+
+            <Swiper
+        autoplay={{
+            delay: 2700,
+            disableOnInteraction: false,
+          }}
+        effect={'fade'}
+        pagination={{
+          dynamicBullets: true,
+        }}
+        loop={true}
+        modules={[EffectFade, Pagination, Autoplay]}
+        className="mySwiper"
+      >
+            {
+                bannerData.map((banner, inx)=> <SwiperSlide key={inx}><BannerSlider banner={banner}></BannerSlider></SwiperSlide>)
+            }
+
+      </Swiper>
+
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Banner/BannerSlider.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+
+const BannerSlider = ({ banner }) => {
+    const {title, description, bg_image} = banner;
+  console.log(banner);
+  return (
+    <div>
+      <div
+        className="hero min-h-[93vh]"
+        style={{
+          backgroundImage:
+            `url(${bg_image})`,
+        }}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-3xl md:text-5xl font-bold text-primary">{title}</h1>
+            <p className="mb-5 text-base md:text-2xl">
+              {description}
+            </p>
+            <button className="bg-primary text-contrast font-medium hover:bg-secondery py-2 px-4 rounded-md ">Book Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+BannerSlider.propTypes = {
+    banner: PropTypes.object.isRequired
+}
+
+export default BannerSlider;
